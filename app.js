73ad5ca5b49513f270f8b4f85f539510e512536f
Barrett Amsrud <barrettamsrud@Barretts-MacBook-Pro.local>
2016-09-23 23:30:47 -0500

    Completed hard and pro mode.  No CSS styling yet

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
 $(document).ready(function() {
 
-  console.log('jQuery is ready!');
+  //console.log('jQuery is ready!');
 
+  var employeeSalaries = {};  //This object will hold employee names as properties, and salaries as values.
+  var expense = 0;  //This will be the value displayed at the bottom of the page
+
+  //This handles all the submission information
   $('#employee_info').on('submit', function(event) {
     event.preventDefault();
-
     var employee = {};
-
     var fields = $('#employee_info').serializeArray();
-
     fields.forEach(function(element, index) {
       employee[element.name] = element.value;
     });
@@ -17,11 +18,37 @@ $(document).ready(function() {
     appendDom(employee);
   });
 
+  //This handles how an employee is added to the list
   function appendDom(emp) {
     var $emp = $('<tr class="employee"></tr>');
     $emp.append(
-    '<td>' + emp['first_name'] + '</td><td>' + emp['last_name'] + '</td><td>' + emp['id_number'] + '</td><td>' + emp['job_title'] + '</td><td>' + emp['salary'] + '</td><td><button class="delete">Delete</button></td>');
+    '<td class="employee_name">' + emp['first_name'] + '</td><td>' + emp['last_name'] + '</td><td>' + emp['id_number'] + '</td><td>' + emp['job_title'] + '</td><td>$' + emp['salary'] + '</td><td><button class="delete">Delete</button></td>');
     $('#employee_data').append($emp);
+    employeeSalaries[emp['first_name']] = parseInt(emp['salary']);
+    console.log(employeeSalaries);
+    calculate();
   }
+
+  //This handles how an employee is removed from the list
+  $('#employee_data').on('click', '.delete', function() {
+    var $row = $(this).closest('tr');
+    var tempEmp = $row.find('.employee_name').text();
+    delete employeeSalaries[tempEmp];
+    console.log(employeeSalaries);
+    $row = (this).closest('tr').remove();
+    calculate();
+  });
+
+
+
+  function calculate() {
+    expense = 0;
+    for (var money in employeeSalaries) {
+      expense += employeeSalaries[money];
+    }
+    expense = Math.round(expense * 100 / 12);
+    $('#total').text('$' + expense / 100);
+    console.log('Expense: ' + expense);
+  };
 
 });
