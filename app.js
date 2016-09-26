c4729d95fd0438d92a7694b78b07035bb2134224
Barrett Amsrud <barrettamsrud@Barretts-MacBook-Pro.local>
2016-09-26 09:33:27 -0500

    changed calculation to search for employee ID instead of names

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
 
   //console.log('jQuery is ready!');
 
-  var employeeSalaries = {};  //This object will hold employee names as properties, and salaries as values.
+  var employeeSalaries = {};  //This object will hold employee IDs as properties, and salaries as values.
   var expense = 0;  //This will be the value displayed at the bottom of the page
 
   //This handles all the submission information
@@ -14,6 +14,7 @@ $(document).ready(function() {
       employee[element.name] = element.value;
     });
     $('#employee_info').find('input[type="text"]').val('');
+    $('#employee_info').find('input[type="number"]').val('');
     appendDom(employee);
   });
 
@@ -21,18 +22,18 @@ $(document).ready(function() {
   function appendDom(emp) {
     var $emp = $('<tr class="employee"></tr>');
     $emp.append(
-    '<td class="employee_name">' + emp['first_name'] + '</td><td>' + emp['last_name'] + '</td><td>' + emp['id_number'] + '</td><td>' + emp['job_title'] + '</td><td>$' + emp['salary'] + '</td><td><button class="delete">Delete</button></td>');
+    '<td>' + emp['first_name'] + '</td><td>' + emp['last_name'] + '</td><td class="employee_id">' + emp['id_number'] + '</td><td>' + emp['job_title'] + '</td><td>$' + emp['salary'] + '</td><td><button class="delete">Delete</button></td>');
     $('#employee_data').append($emp);
-    employeeSalaries[emp['first_name']] = parseFloat(emp['salary']);
+    employeeSalaries[emp['id_number']] = parseFloat(emp['salary']);
     calculate();
   }
 
   //This handles how an employee is removed from the list
   $('#employee_data').on('click', '.delete', function() {
     var $row = $(this).closest('tr');
-    var tempEmp = $row.find('.employee_name').text();   //This variable finds the employee name.
-    delete employeeSalaries[tempEmp];   //Here, the employee name found in line 33 is used to remove the employee from the employeeSalaries object, allowing an accurate calculation
-    $row = (this).closest('tr').remove();
+    var tempEmp = $row.find('.employee_id').text();   //This variable finds the employee ID.
+    delete employeeSalaries[tempEmp];   //Here, the employee ID found in line 33 is used to remove the employee from the employeeSalaries object, allowing an accurate calculation
+    $(this).closest('tr').remove();
     calculate();
   });
 
