1ebcc12fc9e4ad1dcd22251604423997e300a490
Barrett Amsrud <barrettamsrud@gmail.com>
2016-10-18 13:12:12 -0500

    Converted to Angular

--- app.js
+++ app.js
@@ -1,53 +1,42 @@
-$(document).ready(function() {
+var app = angular.module('EmployeeApp', []);
 
-  //console.log('jQuery is ready!');
+app.controller('EmployeeController', function () {
+  console.log('App loaded');
 
-  var employeeSalaries = {};  //This object will hold employee IDs as properties, and salaries as values.
-  var expense = 0;  //This will be the value displayed at the bottom of the page
+  var self = this;
+  var employeeSalaries = {};  //  This object will hold employee IDs as properties, and salaries as values.
+  self.expense = 0;            //  This will be the value displayed at the bottom of the page
+  self.allEmployees = [];      //  To hold all employees for angular
 
-  //This handles all the submission information
-  $('#employee_info').on('submit', function(event) {
-    event.preventDefault();
-    var employee = {};    //This object will hold the submitted user information
-    var fields = $('#employee_info').serializeArray();
-    fields.forEach(function(element, index) {
-      employee[element.name] = element.value;
-    });
-    $('#employee_info').find('input[type="text"]').val('');
-    $('#employee_info').find('input[type="number"]').val('');
-    appendDom(employee);
-  });
-
-  //This handles how an employee is added to the list
-  function appendDom(emp) {
-    var $emp = $('<tr class="employee"></tr>');
-    $emp.append(
-    '<td>' + emp['first_name'] + '</td><td>' + emp['last_name'] + '</td><td class="employee_id">' + emp['id_number'] + '</td><td>' + emp['job_title'] + '</td><td>$' + emp['salary'] + '</td><td><button class="delete">Delete</button></td>');
-    $('#employee_data').append($emp);
-    employeeSalaries[emp['id_number']] = parseFloat(emp['salary']);
+  //  Submission of employee info
+  self.createEmployee = function () {
+    console.log('Submitted employee:', self.employee);
+    //  Make a copy of the object instead of storing the object itself
+    self.allEmployees.push(angular.copy(self.employee));
+    console.log(self.allEmployees);
+    employeeSalaries[self.employee.id] = parseFloat(self.employee.salary);
+    console.log('ID and salaries:', employeeSalaries);
     calculate();
+    self.employee = {};
+  };
+
+  //  Calculate expense
+  function calculate() {
+    self.expense = 0;
+    for (var money in employeeSalaries) {
+      self.expense += employeeSalaries[money];
+    }
+    self.expense = self.expense / 12;
+    return (self.expense = '$' + self.expense.toFixed(2));
   }
 
-  //This handles how an employee is removed from the list
-  $('#employee_data').on('click', '.delete', function() {
-    var $row = $(this).closest('tr');
-    var tempEmp = $row.find('.employee_id').text();   //This variable finds the employee ID.
-    delete employeeSalaries[tempEmp];   //Here, the employee id found in line 33 is used to remove the employee from the employeeSalaries object, allowing an accurate calculation
-    $(this).closest('tr').remove();
+  self.deleteEmployee = function(index) {
+    var person = self.allEmployees[index].id;
+    self.allEmployees.splice(index, 1);
+    console.log('Index:', person);
+    delete employeeSalaries[person];
+    console.log('Salaries', employeeSalaries);
     calculate();
-  });
-
-
-  //Calculates the salary, using the employeeSalaries object.
-  function calculate() {
-    expense = 0;
-    for (var money in employeeSalaries) {
-      expense += employeeSalaries[money];
-    }
-    expense = expense / 12;
-    $('#total').text('$' + expense.toFixed(2));
-    // expense = Math.round(expense * 100 / 12);  //I multiplied by 100 and divided by 12 to keep the 2 decimal places after rounding.
-    // $('#total').text('$' + expense / 100);  //The rounded number is divided by 100.
-  };
+  }
 
 });
